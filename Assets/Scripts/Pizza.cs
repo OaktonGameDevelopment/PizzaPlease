069aff68661557b99b3d8685f71a7c1413d0383b
3amBEANS <66636404+3amBEANS@users.noreply.github.com>
2022-02-20 15:39:15 -0500

    Pizza class
    pizza
    -has toppings, size, and location

---
+++ Assets/Scripts/Pizza.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pizza : MonoBehaviour
+{
+    private string[] possibleToppings = { "Mushrooms", "Anchovies", "Pepperoni", "Bell Peppers", "Pineapple", "Olives" };
+    private string[] possibleLocations = { "The White House", "Blake Avenue", "Andale Drive", "Andover Road", "Dovedale South", "Durham Way North" };
+    private string[] possibleSizes = { "large", "medium", "small" };
+    private string toppings;
+    private string location;
+    private string size;
+
+    //precondition: Must call on pizza class
+    //postcondition: generates random toppings and a random location for a specific pizza.
+    public Pizza()
+    {
+        System.Random rnd = new System.Random();
+        toppings = possibleToppings[rnd.Next(5)];
+        location = possibleLocations[rnd.Next(5)];
+        size = possibleSizes[rnd.Next(2)];
+    }
+
+    public Pizza(string tp, string lc, string sz) {
+        toppings = tp;
+        location = lc;
+        size = sz;
+    }
+
+
+
+}
