462b16abeca2535ee5679f85615f3f5d593d7bfd
VincentChan22 <VincentHChan22@gmail.com>
2022-02-20 18:34:13 -0500

    Fix merge error on Interactable comments
    note: DONT MOVE ASSETS

--- Assets/Interactable.cs
+++ Assets/Interactable.cs
@@ -4,36 +4,32 @@ using UnityEngine;
 
 public class Interactable : MonoBehaviour //Superclass for all interactable objects
 {
-    private RaycastHit objectHit;
-    private Color originalColor;
-    private RaycastHit lastObjectHit;
+    private RaycastHit objectHit;//the RaycastHit info of the current 
+    private Color originalColor;//stores the original color of the objectHit
+    private RaycastHit lastObjectHit;//Stores the RaycastHit info last object hit
     void Start()
     {
+        originalColor = objectHit.collider.GetComponent<MeshRenderer>().material.color;
     }
-    
     void Update()
     {
-        Debug.DrawRay(Camera.main.ScreenPointToRay(Input.mousePosition).origin, Camera.main.ScreenPointToRay(Input.mousePosition).direction * 10, Color.red, 1.0f);
-        if (lastObjectHit.collider != null && (objectHit.collider != null || lastObjectHit.collider != objectHit.collider))
+        //This first line is for debugging where the direction ray will land
+        //Debug.DrawRay(Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0)).origin, Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0)).direction * 10, Color.red, 1.0f);
+
+        if (lastObjectHit.collider != null && (objectHit.collider != null || lastObjectHit.collider != objectHit.collider))//if lastObject hit is initialized and the current object hit is hitting a different object or isnt hitting anything
         {
-            lastObjectHit.collider.GetComponent<MeshRenderer>().material.color = originalColor;
+            lastObjectHit.collider.GetComponent<MeshRenderer>().material.color = originalColor;//revert the yellow color of the lastObjectHit
         }
-        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out objectHit) && objectHit.collider.GetComponentInParent<Interactable>() != null)
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0)), out objectHit) && objectHit.collider.GetComponentInParent<Interactable>() != null)//if the center of the screen is on a interactable object(an object with a script inherited from Interactable)
         {
-            print("HIT");
-            originalColor = objectHit.collider.GetComponent<MeshRenderer>().material.color;
-            objectHit.collider.GetComponent<MeshRenderer>().material.color = Color.yellow;
+
+            objectHit.collider.GetComponent<MeshRenderer>().material.color = Color.yellow;//change the color of the current interactable object to yellow
             lastObjectHit = objectHit;
             if (Input.GetKeyDown("e"))
             {
-                interact();
+                interact();//calls the overrided interact method in the subclass
             }
         }
-        else
-            print("raycast didnt hit");
-
     }
-
     protected virtual void interact() { print("Interactable's interact() fuction was called instead"); }
-
 }
