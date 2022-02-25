d1347165b7c66cb54815bc475284a5a9f668488e
VincentChan22 <VincentHChan22@gmail.com>
2022-02-24 21:22:08 -0500

    Added scripts for both the trashcan and bell
    Note: this does not currently function as the real and fake pizzas were not defined yet. Also there are no audio clips yet.
        /*Precondition: the player must be named "Player"
         *              player must have the Orders_Note attached to it
         *Postcondition: plays a noice whether a real or fake pizza was submitted. When using the scripts make sure:
    Precondition:
    the player must be named "Player"
    player must have the Orders_Note attached to it
    Postcondition: plays a noise whether a real or fake pizza was submitted

---
+++ Assets/Scripts/SubmitFakeOrder.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SubmitFakeOrder : Interactable
+{
+    [SerializeField] private AudioClip goodSound;
+    [SerializeField] private AudioClip badSound;
+    private AudioSource audioSource;
+    private Orders_Note orderScript;
+    /*Precondition: the player must be named "Player"
+    *              player must have the Orders_Note attached to it
+    *Postcondition: plays a noice whether a real or fake pizza was submitted
+    */
+    protected override void interact()
+    {
+        orderScript = GameObject.Find("Player").GetComponent<Orders_Note>();
+        audioSource = gameObject.GetComponent<AudioSource>();
+        orderScript.submit();
+        if (/*orderScript.curPizza.getReal()*/true)
+        {
+            audioSource.clip = badSound;
+            
+        }
+        else
+        {
+            audioSource.clip = goodSound;
+        }
+        audioSource.Play();
+    }
+}
